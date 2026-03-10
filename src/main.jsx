273b959df61b0bd11b1eee00997a916416b6b0cf
bx4g89bh8r-ui <pguerrero82@proton.me>
2026-03-10 16:29:23 +0100

    Actualizar main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ValueRadar from './ValueRadar'
+import ValueRadar from '../ValueRadar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ValueRadar />
   </React.StrictMode>
 )
+
