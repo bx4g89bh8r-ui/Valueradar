b35239c7b04810c221020644e7ac33d4cf1b4ca6
bx4g89bh8r-ui <pguerrero82@proton.me>
2026-03-10 16:26:35 +0100

    Crear main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ValueRadar from './ValueRadar'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ValueRadar />
+  </React.StrictMode>
+)
